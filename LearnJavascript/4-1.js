f1bb1478bf1fa5851b28bda7f34c514fd1edc6d2
Aidar Ibatullin <60510317+skysamurai@users.noreply.github.com>
2023-11-20 18:41:56 +0300

    Update 4-1.js

--- LearnJavascript/4-1.js
+++ LearnJavascript/4-1.js
@@ -5,12 +5,12 @@
 // Измените значение свойства name на Pete.
 // Удалите свойство name из объекта.*/
 
-// const user = {};
-// user.name = "John";
-// user.surname = "Smith";
-// user.name = "Pete";
-// delete user.name;
-// console.log (user.name);
+const user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+console.log (user.name);
 
 // /* Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
 // Должно работать так:
@@ -19,12 +19,12 @@
 // schedule["8:30"] = "get up";
 // alert( isEmpty(schedule) ); // false */
 
-// function isEmpty(obj) {
-//   for (let key in obj) {
-//     return false;
-//   }
-//   return true; 
-// }
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true; 
+}
 
 // // Можно ли изменить объект, объявленный с помощью const? Как вы думаете?
 // const user = {
@@ -37,27 +37,27 @@
 
 // // У нас есть объект, в котором хранятся зарплаты нашей команды:
 
-// let salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130
-// }
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
 
-// let sum = 0;
-// for (let key in salaries){
-//   sum += salaries[key];
-// }
-// console.log (sum);
+let sum = 0;
+for (let key in salaries){
+  sum += salaries[key];
+}
+console.log (sum);
 
 // Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
 // Например:
 // до вызова функции
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
-};
-multiplyNumeric(menu);
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
+// multiplyNumeric(menu);
 
 //  после вызова функции
 // menu = {
